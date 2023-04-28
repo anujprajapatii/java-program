f1bf68dffef3a20694ed2ba2f119cded45023003
Anuj Prajapati <113229736+anujprajapatii@users.noreply.github.com>
2023-04-28 22:52:55 +0530

    oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class main{
+    public static void main(String [] args) {
+        //input
+    Scanner sc = new Scanner(System.in);
+    int x  = sc.nextInt();
+
+    if(x %  2 ==0){
+        System.out.println("even");
+    }else{
+        System.out.println("ODD");
+    }
+        
+    }
+}
